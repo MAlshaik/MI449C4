b31c31676456dfe6d7f0bbbe0735aff90abd3dd2
MAlshaik <alshaik8@msu.edu>
2024-04-20 16:58:23 -0400

    init

--- miapp/app/page.js
+++ miapp/app/page.js
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-24">
-      <h1 className="text-6xl font-bold">Your profile</h1>
-      <Image width={800} height={800} alt="yo" src="/profile.png" />
-      <h1 className="text-4xl font-bold">friends</h1>
-      <div className="flex flex-row gap-28">
-        <Image width={200} height={200} alt="yo" src="/profile.png" />
-        <Image width={200} height={200} alt="yo" src="/profile.png" />
-        <Image width={200} height={200} alt="yo" src="/profile.png" />
-        <Image width={200} height={200} alt="yo" src="/profile.png" />
-        <Image width={200} height={200} alt="yo" src="/profile.png" />
+    <main className="flex min-h-screen flex-col items-center p-24 gap-12">
+      <h1 className="text-4xl font-bold">Your profile</h1>
+      <Image width={400} height={400} alt="yo" src="/profile.png" />
+      <h1 className="text-2xl font-bold">friends</h1>
+      <div className="flex flex-row gap-12">
+        <Image width={100} height={100} alt="yo" src="/profile.png" />
+        <Image width={100} height={100} alt="yo" src="/profile.png" />
+        <Image width={100} height={100} alt="yo" src="/profile.png" />
+        <Image width={100} height={100} alt="yo" src="/profile.png" />
+        <Image width={100} height={100} alt="yo" src="/profile.png" />
       </div>
     </main>
   );
